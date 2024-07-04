08005f606ff86177098c81357b8ca7c06b75dd7d
hstoklosa <hubert.stoklosa23@gmail.com>
2024-07-04 15:10:52 +0100

    remove unnecessary code

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,11 @@ import { TileProvider } from "./context/TileContext";
 import { SpeedProvider } from "./context/SpeedContext";
 
 function App() {
-    window.console.log("XD");
     return (
         <PathfindingProvider>
             <TileProvider>
                 <SpeedProvider>
                     <Navbar />
-                    <h1 className="text-3xl font-bold underline h-screen w-screen bg-blue-500">
-                        Hello world!
-                    </h1>
                 </SpeedProvider>
             </TileProvider>
         </PathfindingProvider>
