970475bd50dc725e24d88c3cddb8584bbd6f0928
hstoklosa <hubert.stoklosa23@gmail.com>
2024-07-06 21:27:40 +0100

    add misc helper functions

---
+++ src/helpers/randInt.ts
@@ -0,0 +1,6 @@
+export const getRandInt = (min: number, max: number): number => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+
+    return Math.floor(Math.random() * (max - min) + min);
+};
